dc174262e0720d633e681a58c6f46ce5f51b1c0a
Fernando1Borges <centraloro@gmail.com>
2016-12-01 22:15:56 -0600

    complete web scraper and hw

--- webScraper/main.py
+++ webScraper/main.py
@@ -1,6 +1,6 @@
 #in terminal run python main.py soccer yes
 
-#lets write a Simple script 
+#lets write a Simple script
 #to get the 20 words and their frequency percentage
 #with highest frequency in an English Wikipedia article.
 #applications are recommender systems, chatbots and NLP, sentiment analysis,
